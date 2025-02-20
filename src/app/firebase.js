bd3921fef980e75bd5217d05fac2eb83afdbfe2f
SmitPatel-31 <patel.smit3131@gmail.com>
2025-02-20 13:06:28 -0500

    basic routes added

---
+++ src/app/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCkjBAPj9uZpT7zHr9ZD36MwjblMlslEa8",
+  authDomain: "codial-3131.firebaseapp.com",
+  projectId: "codial-3131",
+  storageBucket: "codial-3131.firebasestorage.app",
+  messagingSenderId: "128796495819",
+  appId: "1:128796495819:web:84b5b993da1ffa105df04e",
+  measurementId: "G-32E063SE4F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
