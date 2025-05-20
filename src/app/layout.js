fd6c3765a5af8a35cc15031d8b95347108f7fcee
SmitPatel-31 <patel.smit3131@gmail.com>
2025-05-20 13:27:03 -0400

    added SpeedInsight Vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 export const metadata = {
@@ -14,6 +14,8 @@ export default function RootLayout({ children }) {
       <body>
         {children}
       </body>
+      <SpeedInsights/>
     </html>
+    
   );
 }
